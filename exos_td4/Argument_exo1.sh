4bf53f8f195dee3dba3664928d8ab511ab07bb07
Zhongjie LI <lzj44010@gmail.com>
2023-10-16 17:19:50 +0200

    exo4_td4

---
+++ exos_td4/Argument_exo1.sh
@@ -0,0 +1,13 @@
+echo "Entrez l'année (2016, 2017, 2018) :"
+read year
+cd /Users/zhongjie/Documents/PPE1-2023/Fichiers/ANN/$year
+cat $year*.ann | egrep "$1" | wc -l
+
+cd /Users/zhongjie/Documents/PPE1-2023/Fichiers/ANN/2016
+cat 2016*.ann | egrep "$1" | wc -l
+
+cd /Users/zhongjie/Documents/PPE1-2023/Fichiers/ANN/2017
+cat 2017*.ann | egrep "$1" | wc -l
+
+cd /Users/zhongjie/Documents/PPE1-2023/Fichiers/ANN/2018
+cat 2018*.ann | egrep "$1" | wc -l
---
+++ exos_td4/Argument_exo2.sh
@@ -0,0 +1,2 @@
+cd /Users/zhongjie/Documents/PPE1-2023/Fichiers/ANN/*
+cat *.ann | egrep "Location" | cut -f 3 | sort | uniq -c | sort -nr | head 10
