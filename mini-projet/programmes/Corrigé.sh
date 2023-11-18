80520325b3ea5ea97e2746e86978ed1ea21234be
Zhongjie LI <lzj44010@gmail.com>
2023-11-18 15:55:17 +0100

    presentation&tableau

--- mini-projet/programmes/Corrigé.sh
+++ mini-projet/programmes/Corrigé.sh
@@ -1,27 +1,30 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 1 ]; 
+if [[ $# -ne 1 ]]; 
 then
     echo "Il faut un argument qui est le nom complet du fichier texte"
-else
+    exit
+fi
 
-    URLS=$1
-
-    if [ ! -f $URLS ]
+URLS=$1
+if [ ! -f $URLS ]
     then
         echo "On attend un fichier, pas un dossier"
         exit
-    fi
-
-    lineno=1
-    while read -r line;
-    do
-        response=$(curl -s -I -L -w "%{http_code}" -o /dev/null $line)
-        charset=$(curl -s -I -L -w "%{content_type}" -o /dev/null $line | grep -E -o "charset=\S+" | cut -d"=" -f2 | tail -1)
-        echo -e "$lineno\t$line\t$response\t$charset"
-        lineno=$(expr $lineno + 1)
-    done < $URLS
 fi
+
+lineno=1
+html_file="./tableau.html"
+echo "<html><head></head><body>" > $html_file
+echo "<table border='1'>" >> $html_file
+while read -r line;
+do
+    response=$(curl -s -I -L -w "%{http_code}" -o /dev/null $line)
+    charset=$(curl -s -I -L -w "%{content_type}" -o /dev/null $line | grep -E -o "charset=\S+" | cut -d"=" -f2 | tail -1)
+    echo "<tr><td>$lineno</td><td>$line</td><td>$response</td><td>$charset</td></tr>" >> $html_file
+    lineno=$(expr $lineno + 1)
+done < $URLS
+echo "</table></body></html>" >> $html_file
 
 # Pour codes HTTP :
 # Méthode 1 : curl -sI & head -1 | egrep -o "[0-9]{3}" ou curl -sI head -1 | cut -d" " -f2
@@ -30,3 +33,5 @@ fi
 
 # Pour codes charset :
 # curl -s -I -w "%{content_type}" -o /dev/null $line | egrep -o "charset=\S+" (un non caractère d'espace) | cut -d"=" -f2 (deuxième colonne) | tail -n 1
+
+
